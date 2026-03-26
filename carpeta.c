c2e53996c3e1e1a912313d58fd2edc12709fa6f7
daniel.renau <daniel.renau@a2-s109-79.upc.edu>
2026-03-26 11:53:23 +0100

    carpeta.c començada

--- carpeta.c
+++ carpeta.c
@@ -1,5 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/file.c to edit this template
- */
+#include <stdio.h>
+#include <stdint.h>
+#include "carpeta.h"
+
+t_data llegeix_nova_data(){
+
+    int dia, mes, any;
+    
+    printf("\nData (format: dd/mm/aaaa): ");
+    scanf("%d/%d/%d", &dia, &mes, &any);
+}
+
+void mostra_data(t_data data){
+
+}
+
+t_carpeta llegeix_nova_carpeta(){
+
+}
+
+void mostra_carpeta(t_carpeta car){
+
+}
+
+t_data * cerca_data(t_carpeta *car, t_data data){
+
+}
+
+int insereix_nova_data(t_carpeta *car, t_data data){
+
+}
+
+int insereix_nova_tasca(t_data *data, t_tasca tas){
+
+}
+
+t_tasca * cerca_tasca(t_data *data, char titol[MAX_C]){
+
+}
+
+int elimina_tasca(t_data *data, char titol[MAX_C]){
+
+}
+
+int neteja_dates_buides(t_carpeta *car){
+
+}
 
